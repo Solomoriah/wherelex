77f2396dcaec4ac58ad5ed2c70fc8f12dbc82449
Chris Gonnerman <chris@gonnerman.org>
2018-12-02 13:41:53 -0600

    Changed <=> to = due to some operational issues; can't remember exactly what.

--- wherelex.py
+++ wherelex.py
@@ -4,13 +4,13 @@
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # Redistributions of source code must retain the above copyright notice, this
 # list of conditions and the following disclaimer. Redistributions in binary form
 # must reproduce the above copyright notice, this list of conditions and the
 # following disclaimer in the documentation and/or other materials provided with
 # the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -20,7 +20,7 @@
 # SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 """
     wherelex.py
@@ -109,7 +109,7 @@ def interpreter(s, prefix = None):
                 s = s[2:]
             else:
                 if s[0] == '=':
-                    expr.append("<=>")
+                    expr.append("=")
                 else:
                     expr.append(s[0])
                 s = s[1:]
@@ -139,7 +139,7 @@ def interpreter(s, prefix = None):
         else:
             raise AnalyzerError("Syntax error near '%s'" % s[:20])
         s = s.strip()
-                   
+
     return " ".join(expr), values
 
 
@@ -180,5 +180,18 @@ if __name__ == "__main__":
         print `values`
     except:
         print "Error detected."
+
+    print "good parse, but won't work - function call"
+    expression = """
+        (mydate = sleep(90))
+    """
+
+    try:
+        expr, values = interpreter(expression)
+        print expr
+        print `values`
+    except:
+        print "Error detected."
+
 
 # end of file.
